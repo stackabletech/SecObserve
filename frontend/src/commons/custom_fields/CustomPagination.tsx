b27c3e58b5a2c14bb74be09839585a9b1a95971d
dervoeti <lukas.voetmand@stackable.tech>
2024-06-04 11:41:50 +0200

    fix: limit pagination to 2000 since 10000 is practically unusable

--- frontend/src/commons/custom_fields/CustomPagination.tsx
+++ frontend/src/commons/custom_fields/CustomPagination.tsx
@@ -1,3 +1,3 @@
 import { Pagination } from "react-admin";
 
-export const CustomPagination = () => <Pagination rowsPerPageOptions={[10, 25, 50, 100, 500, 10000]} />;
+export const CustomPagination = () => <Pagination rowsPerPageOptions={[10, 25, 50, 100, 500, 2000]} />;
