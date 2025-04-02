cc00e0970752278cda428793f59a2528590c8c7c
dervoeti <lukas.krug@stackable.tech>
2025-04-02 19:46:25 +0200

    chore: aligned observation list with upstream state

--- frontend/src/core/observations/ObservationList.tsx
+++ frontend/src/core/observations/ObservationList.tsx
@@ -1,3 +1,4 @@
+import { Stack } from "@mui/material";
 import { Fragment } from "react";
 import {
     AutocompleteInput,
@@ -5,9 +6,8 @@ import {
     ChipField,
     DatagridConfigurable,
     FilterButton,
-    FilterForm,
     FunctionField,
-    ListContextProvider,
+    List,
     NullableBooleanInput,
     NumberField,
     ReferenceInput,
@@ -15,15 +15,15 @@ import {
     TextField,
     TextInput,
     TopToolbar,
-    useListController,
 } from "react-admin";
 
 import observations from ".";
 import { CustomPagination } from "../../commons/custom_fields/CustomPagination";
 import { SeverityField } from "../../commons/custom_fields/SeverityField";
 import { humanReadableDate } from "../../commons/functions";
+import { feature_exploit_information } from "../../commons/functions";
 import ListHeader from "../../commons/layout/ListHeader";
-import { AutocompleteInputMedium, NullableBooleanInputWide } from "../../commons/layout/themes";
+import { AutocompleteInputMedium, AutocompleteInputWide } from "../../commons/layout/themes";
 import { getSettingListSize } from "../../commons/user_settings/functions";
 import {
     AGE_CHOICES,
@@ -37,51 +37,77 @@ import ObservationBulkAssessment from "./ObservationBulkAssessment";
 import ObservationExpand from "./ObservationExpand";
 import { IDENTIFIER_OBSERVATION_LIST, setListIdentifier } from "./functions";
 
-const listFilters = () => [
-    <ReferenceInput
-        source="product"
-        reference="products"
-        sort={{ field: "name", order: "ASC" }}
-        queryOptions={{ meta: { api_resource: "product_names" } }}
-        alwaysOn
-    >
-        <AutocompleteInputMedium optionText="name" />
-    </ReferenceInput>,
-    <ReferenceInput
-        source="product_group"
-        reference="product_groups"
-        sort={{ field: "name", order: "ASC" }}
-        queryOptions={{ meta: { api_resource: "product_group_names" } }}
-        alwaysOn
-    >
-        <AutocompleteInputMedium optionText="name" />
-    </ReferenceInput>,
-    <TextInput source="branch_name" label="Branch / Version name" alwaysOn />,
-    <TextInput source="title" alwaysOn />,
-    <AutocompleteInput source="current_severity" label="Severity" choices={OBSERVATION_SEVERITY_CHOICES} alwaysOn />,
-    <AutocompleteInput source="current_status" label="Status" choices={OBSERVATION_STATUS_CHOICES} alwaysOn />,
-    // <ReferenceInput label="Service" source="origin_service" reference="services" sort={{ field: "name", order: "ASC" }}>
-    //     <AutocompleteInputWide label="Service" optionText="name_with_product" />
-    // </ReferenceInput>,
-    <TextInput source="origin_component_name_version" label="Component" />,
-    <TextInput source="origin_docker_image_name_tag_short" label="Container" />,
-    // <TextInput source="origin_component_location" label="Component location" />,
-    // <TextInput source="origin_endpoint_hostname" label="Host" />,
-    // <TextInput source="origin_source_file" label="Source" />,
-    // <TextInput source="origin_cloud_qualified_resource" label="Resource" />,
-    // <TextInput source="origin_kubernetes_qualified_resource" label="Kubernetes resource" />,
-    <TextInput source="scanner" />,
-    <AutocompleteInputMedium source="age" choices={AGE_CHOICES} />,
-    <NullableBooleanInput source="has_potential_duplicates" label="Duplicates" />,
-    <NullableBooleanInputWide source="patch_available" label="Patch available" alwaysOn />,
-    <NullableBooleanInput source="cve_known_exploited" label="CVE exploited" alwaysOn />,
-    <AutocompleteInput
-        source="origin_component_purl_type"
-        label="Component type"
-        choices={PURL_TYPE_CHOICES}
-        alwaysOn
-    />,
-];
+function listFilters() {
+    const filters = [];
+    filters.push(
+        <ReferenceInput
+            source="product"
+            reference="products"
+            sort={{ field: "name", order: "ASC" }}
+            queryOptions={{ meta: { api_resource: "product_names" } }}
+            alwaysOn
+        >
+            <AutocompleteInputMedium optionText="name" />
+        </ReferenceInput>,
+        <ReferenceInput
+            source="product_group"
+            reference="product_groups"
+            sort={{ field: "name", order: "ASC" }}
+            queryOptions={{ meta: { api_resource: "product_group_names" } }}
+            alwaysOn
+        >
+            <AutocompleteInputMedium optionText="name" />
+        </ReferenceInput>,
+        <TextInput source="branch_name" label="Branch / Version name" alwaysOn />,
+        <TextInput source="title" alwaysOn />,
+        <AutocompleteInput
+            source="current_severity"
+            label="Severity"
+            choices={OBSERVATION_SEVERITY_CHOICES}
+            alwaysOn
+        />,
+        <AutocompleteInput source="current_status" label="Status" choices={OBSERVATION_STATUS_CHOICES} alwaysOn />,
+        // <ReferenceInput
+        //     label="Service"
+        //     source="origin_service"
+        //     reference="services"
+        //     sort={{ field: "name", order: "ASC" }}
+        // >
+        //     <AutocompleteInputWide label="Service" optionText="name_with_product" />
+        // </ReferenceInput>,
+        <TextInput source="origin_component_name_version" label="Component" />
+    );
+    if (feature_exploit_information()) {
+        filters.push(<NullableBooleanInput source="cve_known_exploited" label="CVE exploited" alwaysOn />);
+    }
+    filters.push(
+        <TextInput source="origin_docker_image_name_tag_short" label="Container" />,
+        // <TextInput source="origin_endpoint_hostname" label="Host" />,
+        // <TextInput source="origin_source_file" label="Source" />,
+        // <TextInput source="origin_cloud_qualified_resource" label="Cloud resource" />,
+        // <TextInput source="origin_kubernetes_qualified_resource" label="Kubernetes resource" />,
+        <TextInput source="scanner" alwaysOn />,
+        <AutocompleteInputMedium source="age" choices={AGE_CHOICES} alwaysOn />,
+        <NullableBooleanInput source="has_potential_duplicates" label="Duplicates" alwaysOn />,
+        <NullableBooleanInput source="patch_available" label="Patch available" alwaysOn />,
+        <AutocompleteInput
+            source="origin_component_purl_type"
+            label="Component type"
+            choices={PURL_TYPE_CHOICES}
+            alwaysOn
+        />
+    );
+    return filters;
+}
+
+const ListActions = () => (
+    <TopToolbar>
+        <Stack spacing={0.5} alignItems="flex-start">
+            <SelectColumnsButton />
+            <FilterButton />
+        </Stack>
+    </TopToolbar>
+);
 
 const BulkActionButtons = () => (
     <Fragment>
@@ -91,92 +117,69 @@ const BulkActionButtons = () => (
 
 const ObservationList = () => {
     setListIdentifier(IDENTIFIER_OBSERVATION_LIST);
-    const listContext = useListController({
-        filterDefaultValues: { current_status: OBSERVATION_STATUS_OPEN },
-        perPage: 25,
-        resource: "observations",
-        sort: { field: "current_severity", order: "ASC" },
-        storeKey: "observations.list",
-        disableSyncWithLocation: false,
-        debounce: 700,
-    });
-    if (listContext.isLoading) {
-        return <div>Loading...</div>;
-    }
-
-    // hack to sync parameters to location URL if they are loaded from the store
-    if (listContext.sort && !document.location.hash.match(/#\/observations.*\?/)) {
-        listContext.setSort(listContext.sort);
-    }
 
     return (
         <Fragment>
             <ListHeader icon={observations.icon} title="Observations" />
-            <ListContextProvider value={listContext}>
-                <div style={{ width: "100%", marginTop: 1 }}>
-                    <TopToolbar>
-                        <FilterForm filters={listFilters()} />
-                        <FilterButton filters={listFilters()} />
-                        <SelectColumnsButton preferenceKey="observations.list" />
-                    </TopToolbar>
-                    <DatagridConfigurable
-                        size={getSettingListSize()}
-                        omit={["scanner_name", "has_potential_duplicates"]}
-                        rowClick="show"
-                        bulkActionButtons={<BulkActionButtons />}
-                        preferenceKey="observations.list"
-                        expand={<ObservationExpand />}
-                        expandSingle
-                    >
-                        <TextField source="product_data.name" label="Product" />
-                        <TextField source="product_data.product_group_name" label="Group" />
-                        <TextField source="branch_name" label="Branch / Version" />
-                        <TextField source="title" />
-                        <SeverityField source="current_severity" label="Severity" />
-                        <ChipField source="current_status" label="Status" />
-                        <NumberField source="epss_score" label="EPSS" />
-                        <NumberField source="upgrade_impact_score" label="Upgrade Impact Score" />
-                        {/* <TextField source="origin_service_name" label="Service" /> */}
-                        <TextField
-                            source="origin_component_name_version"
-                            label="Component"
-                            sx={{ wordBreak: "break-word" }}
-                        />
-                        <TextField
-                            source="origin_docker_image_name_tag_short"
-                            label="Container"
-                            sx={{ wordBreak: "break-word" }}
-                        />
-                        {/*
-                        <TextField
-                            source="origin_component_location"
-                            label="Component location"
-                            sx={{ wordBreak: "break-word" }}
-                        />
-                        <TextField source="origin_endpoint_hostname" label="Host" sx={{ wordBreak: "break-word" }} />
-                        <TextField source="origin_source_file" label="Source" sx={{ wordBreak: "break-word" }} />
-                        <TextField
-                            source="origin_cloud_qualified_resource"
-                            label="Cloud res."
-                            sx={{ wordBreak: "break-word" }}
-                        />
-                        <TextField
-                            source="origin_kubernetes_qualified_resource"
-                            label="Kube. res."
-                            sx={{ wordBreak: "break-word" }}
-                        /> */}
-                        <TextField source="scanner_name" label="Scanner" />
-                        <FunctionField<Observation>
-                            label="Age"
-                            sortBy="last_observation_log"
-                            render={(record) => (record ? humanReadableDate(record.last_observation_log) : "")}
-                        />
-                        <BooleanField source="has_potential_duplicates" label="Dupl." />
-                        <BooleanField source="patch_available" label="Patch" />
-                    </DatagridConfigurable>
-                    <CustomPagination />
-                </div>
-            </ListContextProvider>
+            <List
+                perPage={25}
+                pagination={<CustomPagination />}
+                filters={listFilters()}
+                sort={{ field: "current_severity", order: "ASC" }}
+                filterDefaultValues={{ current_status: OBSERVATION_STATUS_OPEN }}
+                disableSyncWithLocation={false}
+                storeKey="observations.list"
+                actions={<ListActions />}
+                sx={{ marginTop: 1 }}
+            >
+                <DatagridConfigurable
+                    size={getSettingListSize()}
+                    rowClick="show"
+                    bulkActionButtons={<BulkActionButtons />}
+                    expand={<ObservationExpand />}
+                    expandSingle
+                >
+                    <TextField source="product_data.name" label="Product" />
+                    <TextField source="product_data.product_group_name" label="Group" />
+                    <TextField source="branch_name" label="Branch / Version" />
+                    <TextField source="title" />
+                    <SeverityField label="Severity" source="current_severity" />
+                    <ChipField source="current_status" label="Status" />
+                    <NumberField source="epss_score" label="EPSS" />
+                    <NumberField source="upgrade_impact_score" label="Upgrade Impact Score" />
+                    {/* <TextField source="origin_service_name" label="Service" /> */}
+                    <TextField
+                        source="origin_component_name_version"
+                        label="Component"
+                        sx={{ wordBreak: "break-word" }}
+                    />
+                    <TextField
+                        source="origin_docker_image_name_tag_short"
+                        label="Container"
+                        sx={{ wordBreak: "break-word" }}
+                    />
+                    {/* <TextField source="origin_endpoint_hostname" label="Host" sx={{ wordBreak: "break-word" }} />
+                    <TextField source="origin_source_file" label="Source" sx={{ wordBreak: "break-word" }} />
+                    <TextField
+                        source="origin_cloud_qualified_resource"
+                        label="Cloud res."
+                        sx={{ wordBreak: "break-word" }}
+                    />
+                    <TextField
+                        source="origin_kubernetes_qualified_resource"
+                        label="Kube. res."
+                        sx={{ wordBreak: "break-word" }}
+                    /> */}
+                    <TextField source="scanner_name" label="Scanner" />
+                    <FunctionField<Observation>
+                        label="Age"
+                        sortBy="last_observation_log"
+                        render={(record) => (record ? humanReadableDate(record.last_observation_log) : "")}
+                    />
+                    <BooleanField source="has_potential_duplicates" label="Dupl." />
+                    <BooleanField source="patch_available" label="Patch" />
+                </DatagridConfigurable>
+            </List>
         </Fragment>
     );
 };
