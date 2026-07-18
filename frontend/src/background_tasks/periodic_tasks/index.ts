451760d95cc909ff8f4ea6cce633b014b748fdda
Stefan Fleckenstein <stefan@fleckenstein.co.uk>
2026-07-18 14:57:03 +0100

    fix: missing frontend changes

--- frontend/src/background_tasks/periodic_tasks/index.ts
+++ frontend/src/background_tasks/periodic_tasks/index.ts
@@ -1,8 +1,5 @@
 import PeriodicTaskIcon from "@mui/icons-material/Schedule";
 
-import PeriodicTaskList from "./PeriodicTaskList";
-
 export default {
-    list: PeriodicTaskList,
     icon: PeriodicTaskIcon,
 };
