d8b041879c077d2f1b6c4822344ff0defe25e342
dervoeti <lukas.krug@stackable.tech>
2026-09-03 09:16:54 +0200

    chore: merge migrations after upstream sync

---
+++ backend/application/commons/migrations/0025_merge_20260902.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("commons", "0023_merge_20260513"),
+        ("commons", "0024_settings_feature_show_product_header_chips"),
+    ]
+
+    operations = []
---
+++ backend/application/core/migrations/0100_merge_20260902.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("core", "0097_alter_observation_log_rejection_remark"),
+        ("core", "0099_initialize_observation_components"),
+    ]
+
+    operations = []
