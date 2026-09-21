5293cef4ee429e57a17e074d6df5dde4c9b355ad
dervoeti <lukas.krug@stackable.tech>
2026-09-21 12:54:37 +0000

    chore: merge migrations after upstream sync

---
+++ backend/application/commons/migrations/0027_merge_20260921.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("commons", "0025_merge_20260902"),
+        ("commons", "0026_settings_vulnerablecode_cache_ttl_hours"),
+    ]
+
+    operations = []
