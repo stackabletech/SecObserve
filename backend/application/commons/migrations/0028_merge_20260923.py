981a6d0db73b30379b8751182932e90e89b1063b
dervoeti <lukas.krug@stackable.tech>
2026-09-23 08:31:46 +0000

    chore: merge commons migrations after upstream sync

---
+++ backend/application/commons/migrations/0028_merge_20260923.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("commons", "0027_merge_20260921"),
+        ("commons", "0027_settings_oidc_api_token_max_authentication_age"),
+    ]
+
+    operations = []
