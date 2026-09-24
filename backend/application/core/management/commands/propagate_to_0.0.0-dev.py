c5daaac9ee88e492ee99c1850736606163dd29c7
Stefan Fleckenstein <stefan@fleckenstein.co.uk>
2026-09-24 13:06:29 +0100

    feat: management command to propagate assessments to 0.0.0-dev

--- backend/application/core/management/commands/propagate_to_0.0.0-dev.py
+++ backend/application/core/management/commands/propagate_to_0.0.0-dev.py
@@ -3,7 +3,6 @@ import traceback
 from typing import Any
 
 from django.core.management.base import BaseCommand
-from django.core.paginator import Paginator
 
 from application.core.models import Observation
 from application.core.services.assessment import set_propagated_assessment_for_new_observation
@@ -14,21 +13,24 @@ logger = logging.getLogger("secobserve.core")
 
 class Command(BaseCommand):
 
-    help = "Propagate assessments to release 26.7"
+    help = "Propagate assessments to release 0.0.0-dev"
 
     def handle(self, *args: Any, **options: Any) -> None:
-        logger.info("Propagating assessments to release 26.7")
+        logger.info("Propagating assessments to release 0.0.0-dev")
 
         try:
             observations = Observation.objects.filter(
-                branch__name__contains="26.7.0", current_status__in=[Status.STATUS_OPEN, Status.STATUS_IN_REVIEW]
+                branch__name__contains="0.0.0-dev", current_status__in=[Status.STATUS_OPEN, Status.STATUS_IN_REVIEW]
             ).order_by("id")
 
-            paginator = Paginator(observations, 1000)
-            for page_number in paginator.page_range:
-                page = paginator.page(page_number)
-                for observation in page.object_list:
+            last_id = 0
+            while True:
+                page = list(observations.filter(id__gt=last_id)[:1000])
+                if not page:
+                    break
+                for observation in page:
                     set_propagated_assessment_for_new_observation(observation)
+                last_id = page[-1].id
 
             logger.info("... finished")
         except Exception as e:
